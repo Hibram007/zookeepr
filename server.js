89a76d902da406cb5097217bfd2bb3417e9c8c1e
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-10-10 17:15:16 -0700

    ability to search zookeeper added

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const fs = require('fs');
 const path = require('path');
 const { animals } = require('./data/animals');
 const express = require('express');
-const { runInNewContext } = require('vm');
+//require() statements will read the index.js files in each of the directories indicated
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 
 const PORT = process.env.PORT || 3001;
 //instantiating server code
@@ -17,137 +20,8 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
-
-function filterByQuery(query, animalsArray) {
-    let personalityTraitsArray = [];
-    // Note that we save the animalsArray as filteredResults here:
-    let filteredResults = animalsArray;
-    if (query.personalityTraits) {
-      // Save personalityTraits as a dedicated array.
-      // If personalityTraits is a string, place it into a new array and save.
-      if (typeof query.personalityTraits === 'string') {
-        personalityTraitsArray = [query.personalityTraits];
-      } else {
-        personalityTraitsArray = query.personalityTraits;
-      }
-      // Loop through each trait in the personalityTraits array:
-      personalityTraitsArray.forEach(trait => {
-        // Check the trait against each animal in the filteredResults array.
-        // Remember, it is initially a copy of the animalsArray,
-        // but here we're updating it for each trait in the .forEach() loop.
-        // For each trait being targeted by the filter, the filteredResults
-        // array will then contain only the entries that contain the trait,
-        // so at the end we'll have an array of animals that have every one 
-        // of the traits when the .forEach() loop is finished.
-        filteredResults = filteredResults.filter(
-          animal => animal.personalityTraits.indexOf(trait) !== -1
-        );
-      });
-    }
-    if (query.diet) {
-      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
-    }
-    if (query.species) {
-      filteredResults = filteredResults.filter(animal => animal.species === query.species);
-    }
-    if (query.name) {
-      filteredResults = filteredResults.filter(animal => animal.name === query.name);
-    }
-    // return the filtered results:
-    return filteredResults;
-  }
-
-  function findById(id, animalsArray) {
-    const result = animalsArray.filter(animal => animal.id === id) [0];
-    return result;
-  }
-
-  // function that accepts POST rout'e req.body value and Animal Array
-  // function to add new animal to the catalog
-  function createNewAnimal(body, animalsArray) {
-    const animal = body;
-    animalsArray.push(animal);
-    fs.writeFileSync(
-      path.join(__dirname, './data/animals.json'),
-      JSON.stringify({ animals: animalsArray }, null, 2)
-    );
-
-    // return finished code to post route for response
-    return animal;
-  }
-// to validate data 
-  function validateAnimal(animal) {
-    if (!animal.name || typeof animal.name !== 'string') {
-      return false;
-    }
-    if (!animal.species || typeof animal.species !== 'string') {
-      return false;
-    }
-    if (!animal.diet || typeof animal.diet !== 'string') {
-      return false;
-    }
-    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
-      return false;
-    }
-    return true;
-  }
-// first arguement is the route client will fetch from
-// second argument is a callback function, executes everytie the route is accessed with a GET request.
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-        //filterbyQuery allows selection of specific data
-        results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-});
-
-//param route - 2nd GET request 
-// res.send() will send 404 error if resource is not found 
-app.get('/api/animals/:id', (req, res) => {
-  const result = findById(req.params.id, animals);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
-});
-
-// route to accept data and store on server
-app.post('/api/animals', (req, res) => {
- // set id based on what the next index of the array will be
- req.body.id = animals.length.toString();
-
- // if any data in req.body is incorrect, send 400 error back
- if (!validateAnimal(req.body)) {
-  res.status(400).send('The animal is not properly formatted.');
-} else {
-
- // add animal to json file and animals array in this function
- const animal = createNewAnimal(req.body, animals);
-
-  //sends data back to the client
-  res.json(req.body);
-  }
-});
-
-// route adding index html file - sending response data into the index.html file that will be displayed
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-//route for animal.html to be on server
-app.get('/animals', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-// route for zookeeper html
-app.get('/zookeepers', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-});
-// wildcard route for non-exiting routes - error catching mech.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
